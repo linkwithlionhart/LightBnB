a3f58a0bca5fae4f29c72bc24098a98bb93c2113
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-08 13:40:40 -0700

    Add rates and guest review tables to migration schema

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
 
 -- Create users table.
 CREATE TABLE users (
@@ -48,5 +50,24 @@ CREATE TABLE property_reviews (
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
+
+-- CREATE rates table.
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night DECIMAL(10, 2) NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+-- CREATE guest reviews table.
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES userd(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating INTEGER CHECK (rating >= 1 AND rating <= 5),
   message TEXT
 );
